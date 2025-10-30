dad3a7e48aebdc8a0d3bed90f34769b4d4ca4336
marcopeg <marco.pegoraro@gmail.com>
2025-10-30 15:38:32 +0100

    fix restaruant

--- src/entities/full/restaurant.ts
+++ src/entities/full/restaurant.ts
@@ -1,3 +1,9 @@
+import { task } from "./task";
+import { log } from "./log";
+import { fact } from "./fact";
+import { document } from "./document";
+import { idea } from "./idea";
+
 export const restaurant = {
   name: "Restaurants",
   icon: "utensils",
@@ -5,51 +11,77 @@ export const restaurant = {
   list: {
     columns: ["cover", "show", "location"],
   },
+  createRelated: [
+    {
+      key: "task",
+      label: "Task",
+      icon: task.icon,
+      create: {
+        title: "New Task for {@this.show}",
+        attributes: {
+          type: "task",
+          linksTo: ["{@this}"],
+        },
+      },
+    },
+    {
+      key: "log",
+      label: "Log",
+      icon: log.icon,
+      create: {
+        title: "{YY}-{MM}-{DD} {hh}.{mm} Log for {@this.show}",
+        attributes: {
+          type: "log",
+          linksTo: ["{@this}"],
+        },
+      },
+    },
+    {
+      key: "fact",
+      label: "Fact",
+      icon: fact.icon,
+      create: {
+        title: "New Fact for {@this.show}",
+        attributes: {
+          type: "fact",
+          linksTo: ["{@this}"],
+        },
+      },
+    },
+    {
+      key: "document",
+      label: "Document",
+      icon: document.icon,
+      create: {
+        title: "Untitled Document for {@this.show}",
+        attributes: {
+          type: "document",
+          linksTo: ["{@this}"],
+        },
+      },
+    },
+    {
+      key: "idea",
+      label: "Idea",
+      icon: idea.icon,
+      create: {
+        title: "New Idea for {@this.show}",
+        attributes: {
+          type: "idea",
+          linksTo: ["{@this}"],
+        },
+      },
+    },
+  ],
   links: [
-    {
-      type: "backlinks",
-      key: "facts",
-      config: {
-        targetType: "fact",
-        properties: ["restaurant"],
-        title: "Facts",
-        icon: "file-text",
-        sort: {
-          strategy: "manual",
-        },
-      },
-    },
-    {
-      type: "backlinks",
-      key: "logs",
-      config: {
-        targetType: "log",
-        properties: ["restaurant"],
-        title: "Logs",
-        icon: "clipboard-list",
-      },
-    },
-    {
-      type: "backlinks",
-      key: "documents",
-      config: {
-        targetType: "document",
-        properties: ["restaurant"],
-        title: "Documents",
-        icon: "paperclip",
-        sort: {
-          strategy: "manual",
-        },
-      },
-    },
     {
       type: "backlinks",
       key: "tasks",
       config: {
         targetType: "task",
-        properties: ["restaurant"],
-        title: "Tasks",
-        icon: "check-square",
+        properties: ["linksTo"],
+        title: task.name,
+        icon: task.icon,
         columns: [
           {
             type: "show",
@@ -65,6 +97,57 @@ export const restaurant = {
         ],
         sort: {
           strategy: "manual",
+        },
+        createEntity: {
+          referenceCreate: "task",
+        },
+      },
+    },
+    {
+      type: "backlinks",
+      key: "logs",
+      config: {
+        targetType: "log",
+        properties: ["linksTo"],
+        title: log.name,
+        icon: log.icon,
+        createEntity: {
+          referenceCreate: "log",
+        },
+      },
+    },
+    {
+      type: "backlinks",
+      key: "links",
+      config: {
+        title: "Links",
+        icon: "layers",
+        find: {
+          query: [
+            {
+              description: "Notes referencing this restaurant",
+              steps: [
+                {
+                  notIn: {
+                    property: "linksTo",
+                    type: ["log", "task"],
+                  },
+                },
+              ],
+            },
+          ],
+        },
+        columns: [
+          { type: "entityIcon" },
+          { type: "show" },
+          { type: "attribute", key: "type", label: "Type" },
+          { type: "date", align: "right" },
+        ],
+        sort: {
+          strategy: "manual",
+        },
+        createEntity: {
+          enabled: false,
         },
       },
     },
