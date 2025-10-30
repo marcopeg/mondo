d96776607678f3277982c2fca068c12d708e2ec2
marcopeg <marco.pegoraro@gmail.com>
2025-10-30 21:24:30 +0100

    project

--- src/entities/full/project.ts
+++ src/entities/full/project.ts
@@ -1,3 +1,10 @@
+import { fact } from "./fact";
+import { log } from "./log";
+import { document } from "./document";
+import { task } from "./task";
+import { idea } from "./idea";
+import { meeting } from "./meeting";
+
 export const project = {
   name: "Projects",
   icon: "folder-git-2",
@@ -5,48 +12,75 @@ export const project = {
     "\ndate: {{date}}\ncompany: []\nteam: []\nparticipants: []\nstatus: draft\n---\n",
   createRelated: [
     {
-      key: "meeting",
-      label: "Meeting",
-      icon: "calendar-plus",
-      panelKey: "meetings",
-      create: {
-        title: "{YY}-{MM}-{DD} {hh}.{mm} with {@this.show}",
-        attributes: {
-          type: "meeting",
-          participants: ["{@this}"],
-        },
-      },
-    },
-    {
       key: "task",
       label: "Task",
-      icon: "check-square",
-      panelKey: "tasks",
+      icon: task.icon,
       create: {
+        title: "New Task for {@this.show}",
         attributes: {
           type: "task",
+          project: ["{@this}"],
         },
       },
     },
     {
       key: "fact",
       label: "Fact",
-      icon: "bookmark-plus",
-      panelKey: "facts",
+      icon: fact.icon,
       create: {
+        title: "New Fact for {@this.show}",
         attributes: {
           type: "fact",
+          project: ["{@this}"],
         },
       },
     },
     {
       key: "log",
       label: "Log",
-      icon: "file-plus",
-      panelKey: "logs",
+      icon: log.icon,
       create: {
+        title: "{YY}-{MM}-{DD} {hh}.{mm} Log for {@this.show}",
         attributes: {
           type: "log",
+          project: ["{@this}"],
+        },
+      },
+    },
+    {
+      key: "meeting",
+      label: "Meeting",
+      icon: meeting.icon,
+      create: {
+        title: "{YY}-{MM}-{DD} {hh}.{mm} with {@this.show}",
+        attributes: {
+          type: "meeting",
+          project: ["{@this}"],
+          participants: ["{@this.participants}"],
+        },
+      },
+    },
+    {
+      key: "document",
+      label: "Document",
+      icon: document.icon,
+      create: {
+        title: "Untitled Document for {@this.show}",
+        attributes: {
+          type: "document",
+          project: ["{@this}"],
+        },
+      },
+    },
+    {
+      key: "idea",
+      label: "Idea",
+      icon: idea.icon,
+      create: {
+        title: "New Idea for {@this.show}",
+        attributes: {
+          type: "idea",
+          project: ["{@this}"],
         },
       },
     },
@@ -57,10 +91,84 @@ export const project = {
   links: [
     {
       type: "backlinks",
+      key: "tasks",
+      config: {
+        targetType: "task",
+        properties: ["project"],
+        title: task.name,
+        icon: task.icon,
+        columns: [
+          {
+            type: "show",
+          },
+          {
+            type: "attribute",
+            key: "status",
+          },
+          {
+            type: "date",
+            align: "right",
+          },
+        ],
+        sort: {
+          strategy: "manual",
+        },
+        createEntity: {
+          referenceCreate: "task",
+        },
+      },
+    },
+    {
+      type: "backlinks",
+      key: "facts",
+      config: {
+        targetType: "fact",
+        properties: ["project"],
+        title: fact.name,
+        icon: fact.icon,
+        sort: {
+          strategy: "manual",
+        },
+        createEntity: {
+          referenceCreate: "fact",
+        },
+      },
+    },
+    {
+      type: "backlinks",
+      key: "logs",
+      config: {
+        targetType: "log",
+        properties: ["project"],
+        title: log.name,
+        icon: log.icon,
+        createEntity: {
+          referenceCreate: "log",
+        },
+      },
+    },
+    {
+      type: "backlinks",
+      key: "documents",
+      config: {
+        targetType: "document",
+        properties: ["project"],
+        title: document.name,
+        icon: document.icon,
+        sort: {
+          strategy: "manual",
+        },
+        createEntity: {
+          referenceCreate: "document",
+        },
+      },
+    },
+    {
+      type: "backlinks",
       key: "meetings",
       config: {
-        title: "Meetings",
-        icon: "calendar",
+        title: meeting.name,
+        icon: meeting.icon,
         targetType: "meeting",
         properties: ["project"],
         filter: {
@@ -96,73 +204,51 @@ export const project = {
           },
         ],
         createEntity: {
-          enabled: true,
-          title: "{YY}-{MM}-{DD} {hh}.{mm} with {@this.show}",
-          attributes: {
-            participants: ["{@this}"],
-          },
+          referenceCreate: "meeting",
         },
       },
     },
     {
       type: "backlinks",
-      key: "facts",
+      key: "link",
       config: {
-        targetType: "fact",
-        properties: ["project"],
-        title: "Facts",
-        icon: "file-text",
+        title: "Links",
+        icon: "layers",
+        find: {
+          query: [
+            {
+              steps: [
+                {
+                  notIn: {
+                    property: "linksTo",
+                    type: ["fact", "task", "log", "document", "meeting"],
+                  },
+                },
+              ],
+            },
+          ],
+        },
         sort: {
           strategy: "manual",
         },
-      },
-    },
-    {
-      type: "backlinks",
-      key: "logs",
-      config: {
-        targetType: "log",
-        properties: ["project"],
-        title: "Logs",
-        icon: "clipboard-list",
-      },
-    },
-    {
-      type: "backlinks",
-      key: "documents",
-      config: {
-        targetType: "document",
-        properties: ["project"],
-        title: "Documents",
-        icon: "paperclip",
-        sort: {
-          strategy: "manual",
-        },
-      },
-    },
-    {
-      type: "backlinks",
-      key: "tasks",
-      config: {
-        targetType: "task",
-        properties: ["project"],
-        title: "Tasks",
-        icon: "check-square",
         columns: [
+          {
+            type: "entityIcon",
+          },
           {
             type: "show",
           },
           {
-            type: "attribute",
-            key: "status",
+            type: "cover",
+            align: "right",
           },
           {
             type: "date",
             align: "right",
           },
         ],
-        sort: {
-          strategy: "manual",
+        createEntity: {
+          enabled: false,
         },
       },
     },
