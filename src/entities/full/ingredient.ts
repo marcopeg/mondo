df57f1a7b38fa942b16b15e464f5b667354eda9d
marcopeg <marco.pegoraro@gmail.com>
2025-10-30 15:45:15 +0100

    Add more entities

--- src/entities/full/ingredient.ts
+++ src/entities/full/ingredient.ts
@@ -1,3 +1,9 @@
+import { task } from "./task";
+import { log } from "./log";
+import { fact } from "./fact";
+import { document } from "./document";
+import { idea } from "./idea";
+
 export const ingredient = {
   name: "Ingredients",
   icon: "carrot",
@@ -5,6 +11,68 @@ export const ingredient = {
   list: {
     columns: ["cover", "show", "category", "supplier"],
   },
+  createRelated: [
+    {
+      key: "task",
+      label: "Task",
+      icon: task.icon,
+      create: {
+        title: "New Task for {@this.show}",
+        attributes: {
+          type: "task",
+          linksTo: ["{@this}"],
+        },
+      },
+    },
+    {
+      key: "log",
+      label: "Log",
+      icon: log.icon,
+      create: {
+        title: "{YY}-{MM}-{DD} {hh}.{mm} Log for {@this.show}",
+        attributes: {
+          type: "log",
+          linksTo: ["{@this}"],
+        },
+      },
+    },
+    {
+      key: "fact",
+      label: "Fact",
+      icon: fact.icon,
+      create: {
+        title: "New Fact for {@this.show}",
+        attributes: {
+          type: "fact",
+          linksTo: ["{@this}"],
+        },
+      },
+    },
+    {
+      key: "document",
+      label: "Document",
+      icon: document.icon,
+      create: {
+        title: "Untitled Document for {@this.show}",
+        attributes: {
+          type: "document",
+          linksTo: ["{@this}"],
+        },
+      },
+    },
+    {
+      key: "idea",
+      label: "Idea",
+      icon: idea.icon,
+      create: {
+        title: "New Idea for {@this.show}",
+        attributes: {
+          type: "idea",
+          linksTo: ["{@this}"],
+        },
+      },
+    },
+  ],
   links: [
     {
       type: "backlinks",
@@ -29,48 +97,12 @@ export const ingredient = {
     },
     {
       type: "backlinks",
-      key: "facts",
-      config: {
-        targetType: "fact",
-        properties: ["ingredients"],
-        title: "Facts",
-        icon: "file-text",
-        sort: {
-          strategy: "manual",
-        },
-      },
-    },
-    {
-      type: "backlinks",
-      key: "logs",
-      config: {
-        targetType: "log",
-        properties: ["ingredients"],
-        title: "Logs",
-        icon: "clipboard-list",
-      },
-    },
-    {
-      type: "backlinks",
-      key: "documents",
-      config: {
-        targetType: "document",
-        properties: ["ingredients"],
-        title: "Documents",
-        icon: "paperclip",
-        sort: {
-          strategy: "manual",
-        },
-      },
-    },
-    {
-      type: "backlinks",
       key: "tasks",
       config: {
         targetType: "task",
-        properties: ["ingredients"],
-        title: "Tasks",
-        icon: "check-square",
+        properties: ["linksTo"],
+        title: task.name,
+        icon: task.icon,
         columns: [
           {
             type: "show",
@@ -86,6 +118,57 @@ export const ingredient = {
         ],
         sort: {
           strategy: "manual",
+        },
+        createEntity: {
+          referenceCreate: "task",
+        },
+      },
+    },
+    {
+      type: "backlinks",
+      key: "logs",
+      config: {
+        targetType: "log",
+        properties: ["linksTo"],
+        title: log.name,
+        icon: log.icon,
+        createEntity: {
+          referenceCreate: "log",
+        },
+      },
+    },
+    {
+      type: "backlinks",
+      key: "links",
+      config: {
+        title: "Links",
+        icon: "layers",
+        find: {
+          query: [
+            {
+              description: "Notes referencing this ingredient",
+              steps: [
+                {
+                  notIn: {
+                    property: "linksTo",
+                    type: ["log", "task"],
+                  },
+                },
+              ],
+            },
+          ],
+        },
+        columns: [
+          { type: "entityIcon" },
+          { type: "show" },
+          { type: "attribute", key: "type", label: "Type" },
+          { type: "date", align: "right" },
+        ],
+        sort: {
+          strategy: "manual",
+        },
+        createEntity: {
+          enabled: false,
         },
       },
     },
--- src/entities/full/show.ts
+++ src/entities/full/show.ts
@@ -1,3 +1,9 @@
+import { task } from "./task";
+import { log } from "./log";
+import { fact } from "./fact";
+import { document } from "./document";
+import { idea } from "./idea";
+
 export const show = {
   name: "Shows",
   icon: "clapperboard",
@@ -5,51 +11,77 @@ export const show = {
   list: {
     columns: ["cover", "show", "format", "status", "platform", "release_date"],
   },
+  createRelated: [
+    {
+      key: "task",
+      label: "Task",
+      icon: task.icon,
+      create: {
+        title: "New Task for {@this.show}",
+        attributes: {
+          type: "task",
+          linksTo: ["{@this}"],
+        },
+      },
+    },
+    {
+      key: "log",
+      label: "Log",
+      icon: log.icon,
+      create: {
+        title: "{YY}-{MM}-{DD} {hh}.{mm} Log for {@this.show}",
+        attributes: {
+          type: "log",
+          linksTo: ["{@this}"],
+        },
+      },
+    },
+    {
+      key: "fact",
+      label: "Fact",
+      icon: fact.icon,
+      create: {
+        title: "New Fact for {@this.show}",
+        attributes: {
+          type: "fact",
+          linksTo: ["{@this}"],
+        },
+      },
+    },
+    {
+      key: "document",
+      label: "Document",
+      icon: document.icon,
+      create: {
+        title: "Untitled Document for {@this.show}",
+        attributes: {
+          type: "document",
+          linksTo: ["{@this}"],
+        },
+      },
+    },
+    {
+      key: "idea",
+      label: "Idea",
+      icon: idea.icon,
+      create: {
+        title: "New Idea for {@this.show}",
+        attributes: {
+          type: "idea",
+          linksTo: ["{@this}"],
+        },
+      },
+    },
+  ],
   links: [
-    {
-      type: "backlinks",
-      key: "facts",
-      config: {
-        targetType: "fact",
-        properties: ["show"],
-        title: "Facts",
-        icon: "file-text",
-        sort: {
-          strategy: "manual",
-        },
-      },
-    },
-    {
-      type: "backlinks",
-      key: "logs",
-      config: {
-        targetType: "log",
-        properties: ["show"],
-        title: "Logs",
-        icon: "clipboard-list",
-      },
-    },
-    {
-      type: "backlinks",
-      key: "documents",
-      config: {
-        targetType: "document",
-        properties: ["show"],
-        title: "Documents",
-        icon: "paperclip",
-        sort: {
-          strategy: "manual",
-        },
-      },
-    },
     {
       type: "backlinks",
       key: "tasks",
       config: {
         targetType: "task",
-        properties: ["show"],
-        title: "Tasks",
-        icon: "check-square",
+        properties: ["linksTo"],
+        title: task.name,
+        icon: task.icon,
         columns: [
           {
             type: "show",
@@ -65,6 +97,57 @@ export const show = {
         ],
         sort: {
           strategy: "manual",
+        },
+        createEntity: {
+          referenceCreate: "task",
+        },
+      },
+    },
+    {
+      type: "backlinks",
+      key: "logs",
+      config: {
+        targetType: "log",
+        properties: ["linksTo"],
+        title: log.name,
+        icon: log.icon,
+        createEntity: {
+          referenceCreate: "log",
+        },
+      },
+    },
+    {
+      type: "backlinks",
+      key: "links",
+      config: {
+        title: "Links",
+        icon: "layers",
+        find: {
+          query: [
+            {
+              description: "Notes referencing this show",
+              steps: [
+                {
+                  notIn: {
+                    property: "linksTo",
+                    type: ["log", "task"],
+                  },
+                },
+              ],
+            },
+          ],
+        },
+        columns: [
+          { type: "entityIcon" },
+          { type: "show" },
+          { type: "attribute", key: "type", label: "Type" },
+          { type: "date", align: "right" },
+        ],
+        sort: {
+          strategy: "manual",
+        },
+        createEntity: {
+          enabled: false,
         },
       },
     },
